7bb15ea7fe47140b8eea590edae9bba96dd0f954
Theo SEGUIER <tseguier@e3r12p11.42.fr>
2014-10-07 19:42:26 +0200

    dico

--- ft_lang.c
+++ ft_lang.c
@@ -6,7 +6,7 @@
 /*   By: tseguier <tseguier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/24 02:07:36 by tseguier          #+#    #+#             */
-/*   Updated: 2014/10/07 14:23:28 by tseguier         ###   ########.fr       */
+/*   Updated: 2014/10/07 19:42:16 by tseguier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,21 +18,21 @@
 
 t_def				g_dictsh[DICT_LEN] =
 {
-	{NULL, 0, 0, tok_cmd, (OPTYPE_OP | EXTYPE_CMDARG)},
-	{NULL, 0, 0, tok_cmdarg, (OPTYPE_OP | EXTYPE_CMDARG)},
-	{NULL, 0, 0, tok_userinput, (OPTYPE_OP | EXTYPE_CMDARG)},
-	{NULL, 0, 0, tok_filename, (OPTYPE_OP | EXTYPE_CMDARG)},
-	{"<", 1, 2, tok_lredir, EXTYPE_FILE},
-	{"<<", 2, 2, tok_llredir, EXTYPE_FILE},
-	{"<<<", 3, 2, tok_lllredir, EXTYPE_STDIN},
-	{">", 1, 2, tok_rredir, EXTYPE_FILE},
-	{">>", 2, 2, tok_rrredir, EXTYPE_FILE},
-	{"|", 1, 2, tok_pipe, EXTYPE_CMD},
-	{"&", 1, 2, tok_bg, OPTYE_TERMSEP | EXTYPE_CMD},
-	{"||", 2, 2, tok_or, EXTYPE_CMD},
-	{"&&", 2, 2, tok_and, EXTYPE_CMD},
-	{";", 1, 2, tok_semicol, ((EXTYPE_CMD) | (EXTYPE_EMPTY))},
-	{"\n", 1, 2, tok_nl, ((EXTYPE_CMD) | (EXTYPE_EMPTY))}
+	{NULL, 0, 0, tok_cmd, (TOK_OP | tok_cmd)},
+	{NULL, 0, 0, tok_cmdarg, (TOK_OP | TOFLAG(tok_cmdarg))},
+	{NULL, 0, 0, tok_userinput, (TOK_OP | TOFLAG(tok_cmdarg))},
+	{NULL, 0, 0, tok_filename, (TOK_OP | TOFLAG(tok_cmdarg))},
+	{"<", 1, 2, tok_lredir, TOFLAG(tok_filename)},
+	{"<<", 2, 2, tok_llredir, TOFLAG(tok_filename)},
+	{"<<<", 3, 2, tok_lllredir, TOFLAG(tok_userinput)},
+	{">", 1, 2, tok_rredir, TOFLAG(tok_filename)},
+	{">>", 2, 2, tok_rrredir, TOFLAG(tok_filename)},
+	{"|", 1, 2, tok_pipe, TOFLAG(tok_cmd)},
+	{"&", 1, 2, tok_bg, TOK_TERMSEP | TOFLAG(tok_cmd)},
+	{"||", 2, 2, tok_or, TOFLAG(tok_cmd)},
+	{"&&", 2, 2, tok_and, TOFLAG(tok_cmd)},
+	{";", 1, 2, tok_semicol, (TOFLAG(tok_cmd) | (TOFLAG(tok_empty)))},
+	{"\n", 1, 2, tok_nl, (TOFLAG(tok_cmd) | (TOFLAG(tok_empty)))}
 };
 
 int					ft_isop(t_dict dict, char *str, int len)
--- ft_lang.h
+++ ft_lang.h
@@ -6,7 +6,7 @@
 /*   By: tseguier <tseguier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/03/22 05:21:00 by tseguier          #+#    #+#             */
-/*   Updated: 2014/10/07 19:01:01 by tseguier         ###   ########.fr       */
+/*   Updated: 2014/10/07 19:35:45 by tseguier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,8 +30,10 @@
 # define TOK_EXPR (TOFLAG(tok_cmd) | TOFLAG(tok_cmdarg) | TOFLAG(tok_userinput) | TOFLAG(tok_filename))
 # define TOK_LREDIR (TOFLAG(tok_lredir) | TOFLAG(tok_llredir) | TOFLAG(tok_lllredir))
 # define TOK_RREDIR (TOFLAG(tok_rredir) | TOFLAG(tok_rrredir))
+# define TOK_REDIR (TOK_LREDIR | TOK_RREDIR)
 # define TOK_LISTSEP (TOFLAG(tok_and) | TOFLAG(tok_or))
 # define TOK_TERMSEP (TOFLAG(tok_semicol) | TOFLAG(tok_nl) | TOFLAG(tok_bg))
+# define TOK_OP (TOK_REDIR | TOK_LISTSEP | TOK_TERMSEP | TOFLAG(tok_pipe))
 typedef struct		s_def
 {
 	char			mark[OP_MAX_LEN];
